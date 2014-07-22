d3f3f0e22325a403a0eb4198a27114b2c9d456cb
Shane Israel <shanemisrael@gmail.com>
2014-07-22 15:25:09 -0600

    Made java.sh friendly with Java 8's new includes layout

--- java.sh
+++ java.sh
@@ -6,12 +6,12 @@ ARCH=`uname -m`
 # set up Java include and library paths for OS X and Linux
 # NOTE: you may need to modify these if your platform uses different locations
 if [ "$OS" == "Darwin" ] ; then
-    javaIncludes="/System/Library/Frameworks/JavaVM.framework/Headers"
+    javaIncludes="-I/System/Library/Frameworks/JavaVM.framework/Headers"
     javaLibs="-dynamiclib -framework JavaVM"
     jniLibName="libwolfssl.jnilib"
     cflags="-DHAVE_ECC"
 elif [ "$OS" == "Linux" ] ; then
-    javaIncludes="/usr/lib/jvm/java-6-openjdk/include"
+    javaIncludes="-I/usr/lib/jvm/java-6-openjdk/include"
     javaLibs="-shared"
     jniLibName="libwolfSSL.so"
     cflags="-DHAVE_ECC -DUSE_FAST_MATH"
@@ -31,10 +31,10 @@ then
     mkdir ./lib
 fi
 
-gcc -DCYASSL_DTLS -Wall -c $fpic $cflags ./native/com_wolfssl_WolfSSL.c -o ./native/com_wolfssl_WolfSSL.o -I$javaIncludes
-gcc -DCYASSL_DTLS -Wall -c $fpic $cflags ./native/com_wolfssl_WolfSSLSession.c -o ./native/com_wolfssl_WolfSSLSession.o -I$javaIncludes
-gcc -DCYASSL_DTLS -Wall -c $fpic $cflags ./native/com_wolfssl_WolfSSLContext.c -o ./native/com_wolfssl_WolfSSLContext.o -I$javaIncludes
-gcc -DCYASSL_DTLS -Wall -c $fpic $cflags ./native/com_wolfssl_wolfcrypt_RSA.c -o ./native/com_wolfssl_wolfcrypt_RSA.o -I$javaIncludes
-gcc -DCYASSL_DTLS -Wall -c $fpic $cflags ./native/com_wolfssl_wolfcrypt_ECC.c -o ./native/com_wolfssl_wolfcrypt_ECC.o -I$javaIncludes
+gcc -DCYASSL_DTLS -Wall -c $fpic $cflags ./native/com_wolfssl_WolfSSL.c -o ./native/com_wolfssl_WolfSSL.o $javaIncludes
+gcc -DCYASSL_DTLS -Wall -c $fpic $cflags ./native/com_wolfssl_WolfSSLSession.c -o ./native/com_wolfssl_WolfSSLSession.o $javaIncludes
+gcc -DCYASSL_DTLS -Wall -c $fpic $cflags ./native/com_wolfssl_WolfSSLContext.c -o ./native/com_wolfssl_WolfSSLContext.o $javaIncludes
+gcc -DCYASSL_DTLS -Wall -c $fpic $cflags ./native/com_wolfssl_wolfcrypt_RSA.c -o ./native/com_wolfssl_wolfcrypt_RSA.o $javaIncludes
+gcc -DCYASSL_DTLS -Wall -c $fpic $cflags ./native/com_wolfssl_wolfcrypt_ECC.c -o ./native/com_wolfssl_wolfcrypt_ECC.o $javaIncludes
 gcc -Wall $javaLibs $cflags -o ./lib/$jniLibName ./native/com_wolfssl_WolfSSL.o ./native/com_wolfssl_WolfSSLSession.o ./native/com_wolfssl_WolfSSLContext.o ./native/com_wolfssl_wolfcrypt_RSA.o ./native/com_wolfssl_wolfcrypt_ECC.o -lcyassl
 
