e40222b5a9cf395473f058079aefa8de0acf17b1
Chris Conlon <chris@wolfssl.com>
2019-02-15 12:43:58 -0800

    add stub classes for SSLSocketFactory and SSLServerSocketFactory

---
+++ src/java/com/wolfssl/provider/jsse/WolfSSLServerSocketFactory.java
@@ -0,0 +1,74 @@
+/* WolfSSLServerSocketFactory.java
+ *
+ * Copyright (C) 2006-2019 wolfSSL Inc.
+ *
+ * This file is part of wolfSSL.
+ *
+ * wolfSSL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+package com.wolfssl.provider.jsse;
+
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+
+import java.io.IOException;
+
+public class WolfSSLServerSocketFactory extends SSLServerSocketFactory {
+
+    private WolfSSLServerSocketFactory() {
+        super();
+    }
+
+    @Override
+    public String[] getDefaultCipherSuites() {
+        /* TODO */
+        return null;
+    }
+
+    @Override
+    public String[] getSupportedCipherSuites() {
+        /* TODO */
+        return null;
+    }
+
+    @Override
+    public ServerSocket createServerSocket() throws IOException {
+        /* TODO */
+        return null;
+    }
+
+    @Override
+    public ServerSocket createServerSocket(int port) throws IOException {
+        /* TODO */
+        return null;
+    }
+
+    @Override
+    public ServerSocket createServerSocket(int port, int backlog)
+        throws IOException {
+        /* TODO */
+        return null;
+    }
+
+    @Override
+    public ServerSocket createServerSocket(int port, int backlog,
+        InetAddress ifAddress) throws IOException {
+        /* TODO */
+        return null;
+    }
+}
+
