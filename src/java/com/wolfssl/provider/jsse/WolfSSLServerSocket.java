311ddd7d8297a400ff27dd2573c3058f51876945
kojo1 <kojo@wolfssl.com>
2020-05-06 07:58:35 +0900

    guard null pointer

--- src/java/com/wolfssl/provider/jsse/WolfSSLServerSocket.java
+++ src/java/com/wolfssl/provider/jsse/WolfSSLServerSocket.java
@@ -159,8 +159,13 @@ public class WolfSSLServerSocket extends SSLServerSocket {
         }
 
         /* sanitize protocol array for unsupported strings */
-        List<String> supported = 
-            Arrays.asList(WolfSSL.getProtocolsMask(context.getOptions()));
+        List<String> supported;
+        if(context != null)
+            supported = Arrays.asList(
+                            WolfSSL.getProtocolsMask(context.getOptions()));
+        else
+            supported = Arrays.asList(WolfSSL.getProtocols());
+                
         for (int i = 0; i < protocols.length; i++) {
             if (!supported.contains(protocols[i])) {
                 throw new IllegalArgumentException("Unsupported protocol: " +
