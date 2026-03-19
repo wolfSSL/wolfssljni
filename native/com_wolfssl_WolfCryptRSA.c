3fd32a8028b9b201682c7f7887fd78d3b4afb73e
Chris Conlon <chris@wolfssl.com>
2026-03-19 17:03:47 -0600

    JNI: Fix RsaKey resource leak on error paths in doVerify and doDec

--- native/com_wolfssl_WolfCryptRSA.c
+++ native/com_wolfssl_WolfCryptRSA.c
@@ -141,7 +141,6 @@ JNIEXPORT jint JNICALL Java_com_wolfssl_WolfCryptRSA_doVerify
                 (unsigned int)outSz, &myKey);
         if (ret < 0) {
             printf("wc_RsaSSL_Verify failed, ret = %d\n", ret);
-            return ret;
         }
     } else {
         printf("wc_RsaPublicKeyDecode failed, ret = %d\n", ret);
@@ -444,7 +443,6 @@ JNIEXPORT jint JNICALL Java_com_wolfssl_WolfCryptRSA_doDec
                 (unsigned int)outSz, &myKey);
         if (ret < 0) {
             printf("wc_RsaPrivateDecrypt failed, ret = %d\n", ret);
-            return ret;
         }
     } else {
         printf("wc_RsaPrivateKeyDecode failed, ret = %d\n", ret);
